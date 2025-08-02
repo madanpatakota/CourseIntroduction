4ce610e81ebe962ac1c1fa52f6faf93d3eea57e5
Madan.patakota <madan.patakota@gmail.com>
2025-08-02 15:01:18 +0530

    slide16

--- src/app/slides/slide16-slide-career-path/slide16-slide-career-path.component.ts
+++ src/app/slides/slide16-slide-career-path/slide16-slide-career-path.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import { registerScrollAnimations } from 'src/app/shared/utils/scroll-antimate';
 
 @Component({
   selector: 'app-slide16-slide-career-path',
   templateUrl: './slide16-slide-career-path.component.html',
   styleUrls: ['./slide16-slide-career-path.component.css']
 })
-export class Slide16SlideCareerPathComponent {
+export class Slide16SlideCareerPathComponent implements AfterViewInit {
+ constructor(private el: ElementRef) {}
 
+  ngAfterViewInit(): void {
+    registerScrollAnimations('.app-slide-wrapper', this.el.nativeElement);
+  }
 }
+
