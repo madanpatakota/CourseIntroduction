7618a1c0796b695ac213bd383c17eaae898f7e05
Madan.patakota <madan.patakota@gmail.com>
2025-08-02 18:03:53 +0530

    down

--- src/app/slides/slide10-trainer/slide10-trainer.component.ts
+++ src/app/slides/slide10-trainer/slide10-trainer.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import { registerScrollAnimations } from 'src/app/shared/utils/scroll-antimate';
 
 @Component({
   selector: 'app-slide10-trainer',
   templateUrl: './slide10-trainer.component.html',
   styleUrls: ['./slide10-trainer.component.css']
 })
-export class Slide10TrainerComponent {
+export class Slide10TrainerComponent  implements AfterViewInit {
+ constructor(private el: ElementRef) {}
 
+  ngAfterViewInit(): void {
+    registerScrollAnimations('.app-slide-wrapper', this.el.nativeElement);
+  }
 }
