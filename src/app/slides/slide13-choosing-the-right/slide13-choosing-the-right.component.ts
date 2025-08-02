0b28a034634601aeb7c95bda2b94f4af9e4436db
Madan.patakota <madan.patakota@gmail.com>
2025-08-02 17:28:22 +0530

    slide13

--- src/app/slides/slide13-choosing-the-right/slide13-choosing-the-right.component.ts
+++ src/app/slides/slide13-choosing-the-right/slide13-choosing-the-right.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import { registerScrollAnimations } from 'src/app/shared/utils/scroll-antimate';
 
 @Component({
   selector: 'app-slide13-choosing-the-right',
   templateUrl: './slide13-choosing-the-right.component.html',
   styleUrls: ['./slide13-choosing-the-right.component.css']
 })
-export class Slide13ChoosingTheRightComponent {
+export class Slide13ChoosingTheRightComponent  implements AfterViewInit {
+ constructor(private el: ElementRef) {}
 
+  ngAfterViewInit(): void {
+    registerScrollAnimations('.app-slide-wrapper', this.el.nativeElement);
+  }
 }
