09aa00de4361283fde3c6ead6b3be63f9a324d1e
Madan.patakota <madan.patakota@gmail.com>
2025-08-02 15:24:54 +0530

    slide20

--- src/app/slides/slide20-before-misard/slide20-before-misard.component.ts
+++ src/app/slides/slide20-before-misard/slide20-before-misard.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import { registerScrollAnimations } from 'src/app/shared/utils/scroll-antimate';
 
 @Component({
   selector: 'app-slide20-before-misard',
   templateUrl: './slide20-before-misard.component.html',
   styleUrls: ['./slide20-before-misard.component.css']
 })
-export class Slide20BeforeMisardComponent {
+export class Slide20BeforeMisardComponent implements AfterViewInit {
+ constructor(private el: ElementRef) {}
 
+  ngAfterViewInit(): void {
+    registerScrollAnimations('.app-slide-wrapper', this.el.nativeElement);
+  }
 }
