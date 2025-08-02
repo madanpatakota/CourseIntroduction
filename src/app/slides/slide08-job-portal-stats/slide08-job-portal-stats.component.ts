dc0f7d75259707ccc86c63bc6334a71161502139
Madan.patakota <madan.patakota@gmail.com>
2025-08-02 14:34:24 +0530

    slide05

--- src/app/slides/slide08-job-portal-stats/slide08-job-portal-stats.component.ts
+++ src/app/slides/slide08-job-portal-stats/slide08-job-portal-stats.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import { registerScrollAnimations } from 'src/app/shared/utils/scroll-antimate';
 
 @Component({
   selector: 'app-slide08-job-portal-stats',
   templateUrl: './slide08-job-portal-stats.component.html',
   styleUrls: ['./slide08-job-portal-stats.component.css']
 })
-export class Slide08JobPortalStatsComponent {
+export class Slide08JobPortalStatsComponent implements AfterViewInit {
+ constructor(private el: ElementRef) {}
 
+  ngAfterViewInit(): void {
+    registerScrollAnimations('.app-slide-wrapper', this.el.nativeElement);
+  }
 }
