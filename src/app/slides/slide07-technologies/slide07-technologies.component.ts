4abeb22ad46039bb1538e3f3576aa9a7aeece468
Madan.patakota <madan.patakota@gmail.com>
2025-08-02 17:35:27 +0530

    07

--- src/app/slides/slide07-technologies/slide07-technologies.component.ts
+++ src/app/slides/slide07-technologies/slide07-technologies.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import { registerScrollAnimations } from 'src/app/shared/utils/scroll-antimate';
 
 @Component({
   selector: 'app-slide07-technologies',
   templateUrl: './slide07-technologies.component.html',
   styleUrls: ['./slide07-technologies.component.css']
 })
-export class Slide07TechnologiesComponent {
+export class Slide07TechnologiesComponent  implements AfterViewInit {
+ constructor(private el: ElementRef) {}
 
+  ngAfterViewInit(): void {
+    registerScrollAnimations('.app-slide-wrapper', this.el.nativeElement);
+  }
 }
+
--- src/app/slides/slide12-what-you-can-develop/slide12-what-you-can-develop.component.ts
+++ src/app/slides/slide12-what-you-can-develop/slide12-what-you-can-develop.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import { registerScrollAnimations } from 'src/app/shared/utils/scroll-antimate';
 
 @Component({
   selector: 'app-slide12-what-you-can-develop',
   templateUrl: './slide12-what-you-can-develop.component.html',
   styleUrls: ['./slide12-what-you-can-develop.component.css']
 })
-export class Slide12WhatYouCanDevelopComponent {
+export class Slide12WhatYouCanDevelopComponent  implements AfterViewInit {
+ constructor(private el: ElementRef) {}
 
+  ngAfterViewInit(): void {
+    registerScrollAnimations('.app-slide-wrapper', this.el.nativeElement);
+  }
 }
