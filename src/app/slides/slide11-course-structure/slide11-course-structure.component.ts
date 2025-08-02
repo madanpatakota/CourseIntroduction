1cfa08f21cb004db44e385aab41a73f5bb2ab2ee
Madan.patakota <madan.patakota@gmail.com>
2025-08-02 17:39:42 +0530

    step

--- src/app/slides/slide11-course-structure/slide11-course-structure.component.ts
+++ src/app/slides/slide11-course-structure/slide11-course-structure.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import { registerScrollAnimations } from 'src/app/shared/utils/scroll-antimate';
 
 @Component({
   selector: 'app-slide11-course-structure',
   templateUrl: './slide11-course-structure.component.html',
   styleUrls: ['./slide11-course-structure.component.css']
 })
-export class Slide11CourseStructureComponent {
+export class Slide11CourseStructureComponent implements AfterViewInit {
+ constructor(private el: ElementRef) {}
 
+  ngAfterViewInit(): void {
+    registerScrollAnimations('.app-slide-wrapper', this.el.nativeElement);
+  }
 }
