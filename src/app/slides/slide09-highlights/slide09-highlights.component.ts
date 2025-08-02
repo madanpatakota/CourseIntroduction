7499dbc7844a006a22df061a7bf8c468185ac7cb
Madan.patakota <madan.patakota@gmail.com>
2025-08-02 18:06:07 +0530

    09

--- src/app/slides/slide09-highlights/slide09-highlights.component.ts
+++ src/app/slides/slide09-highlights/slide09-highlights.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import { registerScrollAnimations } from 'src/app/shared/utils/scroll-antimate';
 
 @Component({
   selector: 'app-slide09-highlights',
   templateUrl: './slide09-highlights.component.html',
   styleUrls: ['./slide09-highlights.component.css']
 })
-export class Slide09HighlightsComponent {
+export class Slide09HighlightsComponent implements AfterViewInit {
+ constructor(private el: ElementRef) {}
 
+  ngAfterViewInit(): void {
+    registerScrollAnimations('.app-slide-wrapper', this.el.nativeElement);
+  }
 }
