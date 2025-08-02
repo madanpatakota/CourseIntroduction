866d0ff35e96e20bcc1274d78f9dd3a67b0e07db
Madan.patakota <madan.patakota@gmail.com>
2025-08-02 14:08:33 +0530

    slid03

--- src/app/slides/slide01-intro/slide01-intro.component.ts
+++ src/app/slides/slide01-intro/slide01-intro.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import { registerScrollAnimations } from 'src/app/shared/utils/scroll-antimate';
 
 @Component({
   selector: 'app-slide01-intro',
   templateUrl: './slide01-intro.component.html',
   styleUrls: ['./slide01-intro.component.css']
 })
-export class Slide01IntroComponent {
+export class Slide01IntroComponent implements AfterViewInit {
 
+ constructor(private el: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    registerScrollAnimations('.app-slide-wrapper', this.el.nativeElement);
+  }
 }
--- src/app/slides/slide03-who-should-join/slide03-who-should-join.component.ts
+++ src/app/slides/slide03-who-should-join/slide03-who-should-join.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import { registerScrollAnimations } from 'src/app/shared/utils/scroll-antimate';
 
 @Component({
   selector: 'app-slide03-who-should-join',
   templateUrl: './slide03-who-should-join.component.html',
   styleUrls: ['./slide03-who-should-join.component.css']
 })
-export class Slide03WhoShouldJoinComponent {
+export class Slide03WhoShouldJoinComponent implements AfterViewInit {
+ constructor(private el: ElementRef) {}
 
+  ngAfterViewInit(): void {
+    registerScrollAnimations('.app-slide-wrapper', this.el.nativeElement);
+  }
 }
