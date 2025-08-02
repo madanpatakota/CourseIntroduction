8aedafb7391218bed457261f70db9c19fc69f3eb
Madan.patakota <madan.patakota@gmail.com>
2025-08-02 17:12:35 +0530

    slide18

--- src/app/slides/slide18-fullcourse-curr-step-by-step/slide18-fullcourse-curr-step-by-step.component.ts
+++ src/app/slides/slide18-fullcourse-curr-step-by-step/slide18-fullcourse-curr-step-by-step.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import { registerScrollAnimations } from 'src/app/shared/utils/scroll-antimate';
 
 @Component({
   selector: 'app-slide18-fullcourse-curr-step-by-step',
   templateUrl: './slide18-fullcourse-curr-step-by-step.component.html',
   styleUrls: ['./slide18-fullcourse-curr-step-by-step.component.css']
 })
-export class Slide18FullcourseCurrStepByStepComponent {
+export class Slide18FullcourseCurrStepByStepComponent implements AfterViewInit {
+ constructor(private el: ElementRef) {}
 
+  ngAfterViewInit(): void {
+    registerScrollAnimations('.app-slide-wrapper', this.el.nativeElement);
+  }
 }
+
--- src/app/slides/slide18-real-time-projects-you-will-work-on/slide18-real-time-projects-you-will-work-on.component.ts
+++ src/app/slides/slide18-real-time-projects-you-will-work-on/slide18-real-time-projects-you-will-work-on.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import { registerScrollAnimations } from 'src/app/shared/utils/scroll-antimate';
 
 @Component({
   selector: 'app-slide18-real-time-projects-you-will-work-on',
   templateUrl: './slide18-real-time-projects-you-will-work-on.component.html',
   styleUrls: ['./slide18-real-time-projects-you-will-work-on.component.css']
 })
-export class Slide18RealTimeProjectsYouWillWorkOnComponent {
+export class Slide18RealTimeProjectsYouWillWorkOnComponent implements AfterViewInit {
+ constructor(private el: ElementRef) {}
 
+  ngAfterViewInit(): void {
+    registerScrollAnimations('.app-slide-wrapper', this.el.nativeElement);
+  }
 }
